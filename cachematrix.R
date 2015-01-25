4a25c09b3bcd8c2149811ee7c0d456fa5aa48d53
nistagm <northrop@yandex.ru>
2015-01-25 13:33:35 +0300

    commit#1
    commit#1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Put matrix x into cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  if (!exists("y")||(x!=y)) {
+    is_inverted<<-0
+    inverted_y<<-NULL
+    y <<- x
+  }
 }
 
 
-## Write a short comment describing this function
+## If matrix x equals matrix y and is previously cached, then return
+## matrix inverted_y which is also cached. Else invert matrix x, put it
+## into cache and set is_enverted=1
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  if((x!=y)||(is_inverted==0)) {
+    inverted_y <<- solve(x)
+    is_inverted <<- 1
+    y <<- x
+  } 
+  inverted_y
 }
